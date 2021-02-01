b969a3dc76dabc00c6afe36f8952454df45613e1
10Al <813008034@qq.com>
2021-02-01 21:05:27 +0800

    added files for zyy

---
+++ c30b9d7057de51a321bc38793375d9d5/script.js
@@ -0,0 +1,24 @@
+let PictureRank = 0;
+let TotalPictures = 6;
+function End()
+{
+    document.getElementById("start").classList.add("fde");
+    setTimeout(Add, 1000);
+}
+function Add()
+{
+    if(PictureRank == 0){
+        document.getElementById("nxt-page").style.visibility = "visible"
+        document.getElementById("bdy").innerHTML += "<div id=\"main\" class=\"card fde-in\"></div>";
+        PictureRank += 1;
+    }
+    else{
+        if(PictureRank < TotalPictures) {
+            PictureRank += 1;
+            document.getElementById("main").style.backgroundImage = `url(/c30b9d7057de51a321bc38793375d9d5/img/${PictureRank}.png)`;
+            if(PictureRank == TotalPictures) {
+                document.getElementById("nxt-page").style.visibility = "hidden";
+            }
+        }
+    }
+}
